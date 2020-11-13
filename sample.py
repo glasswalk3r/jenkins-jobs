2738ffc5ba19f506d65478b6070b0bb9a6d575fe
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2020-11-13 10:45:54 -0300

    More fixes
    Also added sample using python-jenkins.

---
+++ sample.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import jenkins
+from pprint import pprint
+import xmltodict
+
+token = '11890772629890c1dfaf24cb6aa7635e94'
+server = jenkins.Jenkins('http://localhost:8080', username='alceu', password=token)
+#user = server.get_whoami()
+#pprint(f'user: {user}')
+
+for job in server.get_jobs():
+  pprint(xmltodict.parse(server.get_job_config(job['name'])))
+
