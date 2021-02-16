4eba9a5b6881a170632cef4a57f5bc8393a42ed2
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2021-02-15 22:45:33 -0300

    Software requirements update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ['python-jenkins>=1.7.0']
 
 setup_requirements = ['pytest-runner', ]
 
