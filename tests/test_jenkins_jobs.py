f176673c25c68839e37db88c0135d92e134f978f
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2021-02-15 22:41:44 -0300

    Started creating unit tests

--- tests/test_jenkins_jobs.py
+++ tests/test_jenkins_jobs.py
@@ -1,24 +1,16 @@
-#!/usr/bin/env python
-
 """Tests for `jenkins_jobs` package."""
-
+import inspect
 import pytest
-
 
 from jenkins_jobs.jobs import JenkinsJob, PluginBasedJob, PipelineJob, MavenJob, FreestyleJob
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+def test_jenkinsjob_class():
+    assert inspect.isclass(JenkinsJob)
+    methods = ('__init__', '_find_desc', '_find_timer_trigger', '_onliner', '_clean_spec', '__str__')
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    for method in methods:
+        assert hasattr(JenkinsJob, method)
 
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    with pytest.raises(TypeError):
+        JenkinsJob('foobar', {})
