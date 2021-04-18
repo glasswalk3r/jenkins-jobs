9fe87b81079e0f7b1882731f161831f7fa1f4ba0
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2021-04-18 15:49:35 -0300

    Fixed typo for project URL

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/glasswalk3r/jenkins_jobs',
+    url='https://github.com/glasswalk3r/jenkins-jobs',
     version='0.0.1',
     zip_safe=False,
 )
