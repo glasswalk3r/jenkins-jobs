7c5a491d83b577ed80a1cde257869cf621a21af7
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2022-12-20 01:08:40 -0300

    chore: status upgrade

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email='arfreitas@cpan.org',
     python_requires='>=3.5',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
