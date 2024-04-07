1784045b69413e1dc5a3926394a61f5fc99c0404
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2024-04-07 17:34:15 -0300

    chore: Sphinx very outdated version

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = 'alabaster'
 autodoc_default_options = {'members': True, 'undoc-members': True,
                            'private-members': True, 'special-members': True}
