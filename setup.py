ddf6c8fa426d0fb74af2fdd050d96b91089907b8
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2024-04-08 21:06:04 -0300

    chore: updating requirements

--- setup.py
+++ setup.py
@@ -19,17 +19,17 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Alceu Rodrigues de Freitas Junior",
     author_email='arfreitas@cpan.org',
-    python_requires='>=3.5',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     description="Listing all jobs on a Jenkins server with more information \
 than jenkins-cli.jar",
